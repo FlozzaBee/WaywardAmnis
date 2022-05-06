ca776f8f07b3f2d27fa9a1fa7389cc5848dc2280
Flora <flora.barratt@gmail.com>
2022-05-06 20:14:07 +0100

    yassification update Added  - Skybox asset  - Billboard at the beginining  - Enabled bloom

---
+++ Assets/Scripts/EmmisionFlicker.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmmisionFlicker : MonoBehaviour
+{
+    public Material material;
+    public float minOnTime = 0.1f;
+    public float maxOnTime = 1f;
+    public float minOffTime = 0.1f;
+    public float maxOffTime = 2f;
+
+    private void Start()
+    {
+        StartCoroutine(FlickerOn(Random.Range(minOnTime, maxOnTime)));
+    }
+
+    IEnumerator FlickerOn(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        material.EnableKeyword("_EMISSION");
+        StartCoroutine(FlickerOff(Random.Range(minOffTime, maxOffTime)));
+    }
+
+    IEnumerator FlickerOff(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        material.DisableKeyword("_EMISSION");
+        StartCoroutine(FlickerOn(Random.Range(minOnTime, maxOnTime)));
+    }
+}
--- Assets/Scripts/VFXBehaviour.cs
+++ Assets/Scripts/VFXBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FogBehaviour : MonoBehaviour
+public class VFXBehaviour : MonoBehaviour
 {
     public Color startColour;
     public Color EndColour;
